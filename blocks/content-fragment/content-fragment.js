6c5649886fecff5248ce03949a5fbd62ef2b8f0c
joannehoazadobe <joaho@adobe.com>
2025-09-12 16:56:39 -0700

    content block rendering

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -58,7 +58,72 @@ export default async function decorate(block) {
   </div>
 	`;
 
-  block.innerHTML = `<article class="system-style physician system-entry ui-repeater visible" id="PhysicianContent" universal_="true" data-onvisible="visible" data-content="true">
+  block.innerHTML = `
+  <aside class="sub-zone side-zone" id="SubZoneRight" role="presentation"><section class="system-style system-entry no-padding physician ui-repeater visible" id="PhysiciansSideInfo" universal_="true" data-onvisible="visible">
+	
+	<strong class="title-style-1" data-item="i" role="heading" aria-level="2">Tony Abdo,  MD</strong>
+	
+	
+		<ul class="inline-items mar-b-2">
+			
+				<li class="inline-svg flex-middle"><svg viewBox="0 0 36 36" data-use="/cms/svg/site/dv5guqmrkyk.36.svg#check" aria-hidden="true">
+	<path d="M9.204 33.656a17.836 17.836 0 0 1-8.516-10.792a17.75 17.75 0 0 1 1.618-13.64a18.016 18.016 0 0 1 15.716-9.182a18.056 18.056 0 0 1 8.779 2.286a0.964 0.964 0 0 1 0.458 0.59a0.964 0.964 0 0 1-0.087 0.746a0.98 0.98 0 0 1-0.855 0.498a0.922 0.922 0 0 1-0.476-0.128a16.098 16.098 0 0 0-7.83-2.032a16.016 16.016 0 0 0-3.222 0.33a15.915 15.915 0 0 0-10.163 6.864a15.848 15.848 0 0 0-2.334 12.022a16.078 16.078 0 0 0 15.703 12.806a16.182 16.182 0 0 0 3.236-0.332a15.938 15.938 0 0 0 10.172-6.864a15.876 15.876 0 0 0 2.325-12.029a0.956 0.956 0 0 1 0.143-0.73a0.942 0.942 0 0 1 0.621-0.411a0.776 0.776 0 0 1 0.194-0.018a0.97 0.97 0 0 1 0.952 0.774a18.415 18.415 0 0 1 0.363 3.6a18.012 18.012 0 0 1-2.294 8.762a18.038 18.038 0 0 1-15.723 9.182A17.998 17.998-35176.904 0 1 9.204 33.656Zm8.103-11.567l-6.808-6.788a0.984 0.984 0 0 1-0.288-0.693a0.945 0.945 0 0 1 0.288-0.687a0.975 0.975 0 0 1 0.69-0.291a0.956 0.956 0 0 1 0.687 0.291l6.122 6.099l16.329-16.288a0.97 0.97 0 0 1 0.69-0.288a0.969 0.969 0 0 1 0.69 0.28a1.004 1.004 0 0 1 0.288 0.698a0.96 0.96 0 0 1-0.288 0.682l-17.016 16.983a1.008 1.008 0 0 1-0.693 0.28A0.981 0.981-35176.904 0 1 17.31 22.092Z"></path>
+</svg>Accepting New Patients</li>
+			
+			
+		</ul>
+	
+	<figure class="system-card tall" role="presentation" data-item="i">
+		<picture class="">
+			<img class="bg-cover-center" src="/cms/thumbnails/00/400x500//images/physicians/Tony-Abdo-MD.jpg" alt="">
+			
+		</picture>
+	</figure>
+	
+</section><section id="DocScorePhys" class="npr-scores system-style no-padding ui-repeater hide">
+	
+	
+	<div class="npr-scores-cont" data-item="i" data-key="114341">
+		
+		<div class="valid-npi">
+			<div class="ds-summary" data-ds-id="1841555935"></div>
+			<div class="link-cont"><a href="javascript:void(0)" class="link-style-1 view-comments">View All Comments</a></div>
+		</div>
+			<div class="not-npi hide flex-between-middle">
+				<div class="star-cont">
+					<div class="stars"></div>
+					<strong>No Rating Available</strong>
+				</div>
+				<div class="btn-cont">
+					<a href="javascript:void(0)" class="btn small" modalid="1" data-role="modalbtn">Why no Ratings?</a>
+				</div>
+			</div>
+		
+	</div>
+</section><section class="system-style physician-locations locations no-padding full ui-repeater visible" id="PhysicianLocationsContainer" universal_="true" data-onvisible="visible" data-firstname="Tony" data-lastname="Abdo">
+	<header role="presentation">
+		<strong class="title-style-3">Locations</strong>
+	</header>
+	<ul class="system-cards items-1">
+		<li class="item-1" data-item="i" data-key="5393931">
+			<strong class="title-style-5" role="heading" aria-level="3">OU Health Physicians — Cardiology, Pulmonary &amp; Vascular Medicine Clinic</strong>
+			
+				<span class="note-style-1">
+					
+					 Adult Services
+				</span>
+			
+			<address>825 NE 10th, 2E <br>Oklahoma City, OK 73104
+			<br><a href="tel:4052717001" class="phone-link">(405) 271-7001</a></address>
+			
+				<a aria-label="more information about this location" class="link-style-1" href="/find-a-location/ou-health-physicians-cardiology-pulmonary-vascul/">More Information</a>
+			
+			
+		</li>
+	</ul>
+</section></aside>
+
+  <article class="system-style physician system-entry ui-repeater visible" id="PhysicianContent" universal_="true" data-onvisible="visible" data-content="true">
 	
 	
 	<h1 class="hide-1024" data-item="i" data-key="114341" aria-level="2" role="heading">Tony Abdo,  MD</h1>
@@ -129,24 +194,6 @@ export default async function decorate(block) {
 			</li>
 		
 	</ul>
-	
-	
-	<div class="affiliations mar-t-1 content-style hide ui-repeater" id="PhysicianContent_ITM114341_PhysAffiliations" data-item="i" data-key="114341">
-		<strong class="title-style-3">Affiliations</strong>
-		<span data-item="i" data-key="76474">
-			
-			
-				OU Health Physicians – Cardiology, Pulmonary &amp; Vascular Medicine Clinic, 
-			
-		</span><span data-item="i" data-key="76669">
-			
-			
-				OU Health Physicians Building - Oklahoma City
-			
-		</span>
-	</div>
-	
-	
 	
 		<div id="PhysicianContent_ITM114341_PhysEducation" class="phys-education content-style mar-t-3 ui-repeater">
 			<strong class="title-style-2">Health Education</strong>
