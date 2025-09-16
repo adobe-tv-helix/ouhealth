0451f4cdc6fdb8c0b46dbb9f5d3c381f25fd25cc
joannehoazadobe <joaho@adobe.com>
2025-09-15 17:37:02 -0700

    initial add for even listeners for search filters (search by gender) in find a doctor block. div section repeating and not sure where.  added console logging

--- blocks/find-a-doctor/find-a-doctor.js
+++ blocks/find-a-doctor/find-a-doctor.js
@@ -63,7 +63,7 @@ console.log('block config', config);
 					<a href="javascript:void(0);" class="link-style-2 filter-btn more-less" data-type="open" role="button" tabindex="0" aria-expanded="false">
 						<span class="filter-show" data-role="btn"><img src="https://www.ouhealth.com/images/assets/link-arrow-large.png" alt="Show Advanced Filters"> Show Advanced Filters</span></a>
 					<a aria-label="click to clear existing filters" href="javascript:void('ClearAll');" class="link-style-2 clear-filters">
-                        <img src="https://www.ouhealth.com/images/assets/link-arrow-large.png" alt="Clear Filters">&nbsp;
+                        <img src="https://www.ouhealth.com/images/assets/link-arrow-large.png" alt="Clear Filters">
                         Clear Filters
                     </a>
 				</div>
