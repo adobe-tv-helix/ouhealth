a3103a3a740af0708911279a2ee99860c9a1814b
joannehoazadobe <joaho@adobe.com>
2025-09-16 12:16:43 -0700

    added logic to search when name is entered in filter. ONLY executes query when LEAVING the input field. CORS issue

--- blocks/find-a-doctor/find-a-doctor.js
+++ blocks/find-a-doctor/find-a-doctor.js
@@ -274,7 +274,7 @@ async function fetchDoctorsBySelectedName(selectedName) {
 	// const cfList = await executeQuery(url);
 	const url = `${aemPublishUrl}${persistedQuery}`;
 	const variables = {
-	nameValue: 'tony abdo',
+	nameValue: decodeURIComponent(selectedName),
 	ts: Math.random() * 1000
 	};
 	const params = new URLSearchParams(variables).toString();
